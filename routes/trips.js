43e2027921cd29666a542897f1e8720395c42b37
Nessiec86 <nessc86@gmail.com>
2019-02-26 20:40:45 +0100

    Homepage add

--- routes/trips.js
+++ routes/trips.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.get('/index', (req, res, next) => {
+  res.render('trips/home');
 });
 
 module.exports = router;
