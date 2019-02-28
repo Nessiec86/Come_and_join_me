8d863c2ba3a4ed4e2a442069858f9cd525fdf0f6
Nessiec86 <nessc86@gmail.com>
2019-02-28 19:53:27 +0100

    added botton functions

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const saltRounds = 10;
 const router = express.Router();
 //exportar user models
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup');
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('auth/login');
 });
 
 module.exports = router;
