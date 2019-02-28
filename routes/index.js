d1d4abe00024f233c69ed44825525a515dd77356
Nessiec86 <nessc86@gmail.com>
2019-02-28 20:09:17 +0100

    add Sign up function

--- routes/index.js
+++ routes/index.js
@@ -9,10 +9,40 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   res.render('index');
 });
-
+// SIGN UP !
 router.get('/signup', (req, res, next) => {
-  res.render('auth/signup');
+  res.render('auth/signup', /*{ errorMessage: req.flash('error') }*/);
 });
+  
+router.post('/signup', /*middlewares.anonRoute,*/ (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (username === '' || password === '') {
+    return res.redirect('/signup');
+  }
+  User.findOne({ username })
+    .then((user) => {
+      if (user) {
+        //req.flash('error', 'el usuario no existe');
+        res.redirect('/signup');
+      } else {
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+        User.create({ username, password: hashedPassword })
+          .then(() => {
+            res.redirect('/user');
+          }).catch((error) => {
+            next(error);
+          });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+
+
 
 router.get('/login', (req, res, next) => {
   res.render('auth/login');
